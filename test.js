9b1f51d6444b859d1d9690951a4c129cf6521237
eniolastyle <amiolastyle@gmail.com>
2023-04-18 22:22:05 +0100

    testing

--- test.js
+++
@@ -1,7 +0,0 @@
-// - name: Terraform Init
-//         run: terraform init -backend-config="bucket=$TF_STATE_BUCKET_NAME"
-//       - name: Terraform apply
-//         run: |
-//           terraform apply \
-//           -var-file=terraform.tfvars \
-//           -var="public_key=$PUBLIC_SSH_KEY" \
